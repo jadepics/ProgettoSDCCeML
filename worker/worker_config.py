3a050ab6abc396aab93c2fa32ea9da89e9264ad1
Simone2oo2 <simone.manisi002@gmail.com>
2026-05-14 12:40:09 +0200

    multi-workers

--- worker/worker_config.py
+++ worker/worker_config.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Optional
 import os
+from urllib.request import Request, urlopen
 
 from worker.utils.id_utils import generate_worker_id
 
@@ -18,48 +19,62 @@ class WorkerConfig:
 
     @staticmethod
     def from_env() -> "WorkerConfig":
+        worker_id = os.getenv("WORKER_ID") or generate_worker_id()
+
+        bind_host = os.getenv("WORKER_BIND_HOST", "0.0.0.0")
+        port = int(os.getenv("WORKER_PORT", "50061"))
+
+        master_host = os.getenv("MASTER_HOST")
+        if not master_host:
+            raise ValueError("MASTER_HOST is required")
+
+        master_port = int(os.getenv("MASTER_PORT", "50051"))
+
+        artifact_root = os.getenv("ARTIFACT_ROOT", "/mnt/efs/gp_artifacts")
+
+        advertise_host = os.getenv("WORKER_ADVERTISE_HOST")
+        if not advertise_host:
+            advertise_host = WorkerConfig._resolve_private_ip()
+
+        max_workers = int(os.getenv("WORKER_MAX_WORKERS", "16"))
+
         return WorkerConfig(
-
-            worker_id=os.getenv(
-                "WORKER_ID",
-                generate_worker_id()
-            ),
-
-            # corretto mantenere "0.0.0.0"
-            bind_host=os.getenv(
-                "WORKER_BIND_HOST",
-                "0.0.0.0"
-            ),
-
-            #porta di ascolto, unica per istanza di EC2 ma ripetibile per diverse istanze
-            port=int(
-                os.getenv("WORKER_PORT", "50061")
-            ),
-
-            #host privato del master
-            master_host=os.getenv(
-                "MASTER_HOST",
-                "172.31.37.47"
-            ),
-
-            #porta del master che manteniamo sempre 50051
-            master_port=int(
-                os.getenv("MASTER_PORT", "50051")
-            ),
-
-            #root per la scrittura dei files all'interno del sistema
-            artifact_root=os.getenv(
-                "ARTIFACT_ROOT",
-                "/mnt/efs/gp_artifacts"
-            ),
-
-            #Ip privato del worker. Per adesso lo hardcodiamo, poi sarà da modificare in modo che se creiamo diverse istanze ec2, questo venga preso in automatico
-            advertise_host=os.getenv(
-                "WORKER_ADVERTISE_HOST",
-                "172.31.39.5"
-            ),
-
-            max_workers=int(
-                os.getenv("WORKER_MAX_WORKERS", "16")
-            ),
+            worker_id=worker_id,
+            bind_host=bind_host,
+            port=port,
+            master_host=master_host,
+            master_port=master_port,
+            artifact_root=artifact_root,
+            advertise_host=advertise_host,
+            max_workers=max_workers,
         )
+
+    @staticmethod
+    def _resolve_private_ip() -> str:
+        """
+        Prova a recuperare l'IP privato dell'istanza EC2 tramite IMDS.
+        Utile se non vuoi scrivere WORKER_ADVERTISE_HOST a mano.
+        """
+
+        # IMDSv2 token
+        token_req = Request(
+            "http://169.254.169.254/latest/api/token",
+            method="PUT",
+            headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"},
+        )
+
+        try:
+            with urlopen(token_req, timeout=2) as resp:
+                token = resp.read().decode("utf-8")
+
+            meta_req = Request(
+                "http://169.254.169.254/latest/meta-data/local-ipv4",
+                headers={"X-aws-ec2-metadata-token": token},
+            )
+
+            with urlopen(meta_req, timeout=2) as resp:
+                return resp.read().decode("utf-8")
+
+        except Exception:
+            # fallback solo per sviluppo locale/non-EC2
+            return "127.0.0.1"
