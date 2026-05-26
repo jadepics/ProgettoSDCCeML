192f7c156bbcddd44324b6d04d17915e290c102d
Simone2oo2 <simone.manisi002@gmail.com>
2026-05-26 16:19:05 +0200

    Introduzione dell'iperparametro Numero degli alberi come parametro da cli

--- submit_training_classification.py
+++ submit_training_classification.py
@@ -10,6 +10,7 @@ import rf_v2_pb2_grpc as pbgrpc
 def main(
     PrivateIp_Port: str,
     dataset_path: Union[str, Path],
+    n_estimators_total : int,
     dataset_scenario: str = "baseline_original",
     leakage_columns: Optional[List[str]] = None,
 ):
@@ -23,17 +24,25 @@ def main(
         dataset_url=str(dataset_path),
         target_column="diagnosed_diabetes",
         task_type="classification",
-        n_estimators_total=24,
+        dataset_scenario=dataset_scenario,
+        #leakege_scenario non è necessario perché il ds è preso in linea di comando
         validation_ratio=0.2,
         test_ratio=0.2,
         bootstrap=True,
         global_random_seed=42,
+        #HP
+        # profondità degli alberi
         max_depth_candidates=[5],
+        # numero di alberi
+        n_estimators_total=n_estimators_total,
+        # Numero di feature considerate a ogni split.
         max_features_candidates=["sqrt"],
+        # Numero minimo di campioni per fare uno split.
         min_samples_split_candidates=[2],
+        # Numero minimo di esempi in una foglia.
         min_samples_leaf_candidates=[1],
+        # Funzione usata per valutare gli split.
         criterion_candidates=["gini"],
-        dataset_scenario=dataset_scenario,
     )
 
     request.leakage_columns.extend(leakage_columns)
--- submit_training_regression.py
+++ submit_training_regression.py
@@ -4,7 +4,7 @@ import grpc
 import rf_v2_pb2 as pb
 import rf_v2_pb2_grpc as pbgrpc
 
-def main(PrivateIp_Port : str, dataset_path :str):
+def main(PrivateIp_Port : str, dataset_path :Path, n_estimators_total : int):
 
     channel = grpc.insecure_channel(PrivateIp_Port)
     stub = pbgrpc.CoordinatorServiceStub(channel)
@@ -19,15 +19,22 @@ def main(PrivateIp_Port : str, dataset_path :str):
             "diabetes_stage",
             "diabetes_risk_score",
         ],
-        n_estimators_total=24,
         validation_ratio=0.2,
         test_ratio=0.2,
         bootstrap=True,
         global_random_seed=42,
+        #HP
+        #profondità degli alberi
         max_depth_candidates=[5],
+        #numero di alberi
+        n_estimators_total=n_estimators_total,
+        #Numero di feature considerate a ogni split.
         max_features_candidates=["sqrt"],
+        #Numero minimo di campioni per fare uno split.
         min_samples_split_candidates=[2],
+        #Numero minimo di esempi in una foglia.
         min_samples_leaf_candidates=[1],
+        #Funzione usata per valutare gli split.
         criterion_candidates=["squared_error"],
     )
 
